363ef1a6aaf6b9aaba0dfb7656b46387d503b694
saramahmoud2071 <89441343+saramahmoud2071@users.noreply.github.com>
2024-06-30 07:01:02 +0300

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE designer_catalyst_accounts;
+USE designer_catalyst_accounts;
+CREATE TABLE User (id CHAR(10), firstName VARCHAR(30), secondName VARCHAR(30), userName VARCHAR(30),
+email VARCHAR(50), password VARCHAR(50), jobDescription VARCHAR(50));
+CREATE TABLE User (id CHAR(10), firstName VARCHAR(30), secondName VARCHAR(30), userName VARCHAR(30),
+email VARCHAR(50), password VARCHAR(50), jobDescription VARCHAR(50));
